2cd895eec93da82bb851460fa3488ccc437b80a0
Glenn Francis Murray <glenn@metonymous.com>
2011-05-18 20:42:03 +0000

    Google and normal

--- config/initializers/oa-openid.rb
+++ config/initializers/oa-openid.rb
@@ -3,3 +3,4 @@ require 'openid/store/filesystem'
 
 Rails.application.config.middleware.use Rack::Session::Cookie
 Rails.application.config.middleware.use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('/tmp')
+Rails.application.config.middleware.use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('/tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
